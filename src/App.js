e1901f37fc3c608f3a7276a5707672692809abf3
Jani Lamppu <jani.lamppu@gmail.com>
2021-04-24 11:53:05 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,126 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+	LineChart,
+	Line,
+	XAxis,
+	YAxis,
+	CartesianGrid,
+	Tooltip,
+	Legend,
+	ResponsiveContainer,
+} from 'recharts';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { getPricesPerMonth, getPriceForDays } from './utils/dataParser';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [priceData, setPriceData] = useState([]);
+	const [showingData, setShowingData] = useState([]);
+	const [viewMode, setViewMode] = useState('0');
+	const daysToShow = 15;
+
+	useEffect(() => {
+		async function fetch() {
+			const headers = {
+				'x-api-key': process.env.REACT_APP_API_KEY,
+			};
+			const url = `${process.env.REACT_APP_API_URL}/get-prices`;
+			const response = await axios.get(url, { headers });
+			const items = response.data;
+			setPriceData(items);
+		}
+
+		fetch();
+	}, []);
+
+	useEffect(() => {
+		setShowingData(getPreparedData(priceData, viewMode));
+	}, [priceData, viewMode]);
+
+	const getPreparedData = (data, mode) => {
+		if (mode === '0') {
+			// show latest 15 days
+			return getPriceForDays(data, daysToShow);
+		} else if (mode === '1') {
+			// show avg price per month
+			return getPricesPerMonth(data);
+		}
+	};
+
+	const changeViewMode = (event) => {
+		setViewMode(event.target.value);
+	};
+
+	return (
+		<div>
+			<div
+				style={{
+					width: '100%',
+					height: '500px',
+					display: 'flex',
+					alignItems: 'center',
+					justifyContent: 'center',
+					marginTop: '25px',
+				}}
+			>
+				<ResponsiveContainer width="80%">
+					<LineChart data={showingData} width="100%">
+						<CartesianGrid strokeDasharray="1 4" />
+						<XAxis dataKey="date" />
+						<Tooltip
+							contentStyle={{ backgroundColor: '#656970', color: 'white' }}
+						/>
+						<YAxis
+							type="number"
+							tickCount={7}
+							tickFormatter={(item) => `${item.toFixed(2)} €`}
+							domain={[1.0, 2]}
+						/>
+						<Tooltip />
+						<Legend layout="horizontal" iconSize="5" iconType="dot" />
+
+						<Line
+							type="monotone"
+							name="Maakaasu, alue 1"
+							dataKey="naturalgas.zone1"
+							stroke="orange"
+						/>
+						<Line
+							type="monotone"
+							name="Maakaasu, alue 2"
+							dataKey="naturalgas.zone2"
+							stroke="lightblue"
+						/>
+						<Line
+							type="monotone"
+							name="Biokaasu, alue 1"
+							dataKey="biogas.zone1"
+							stroke="lightgreen"
+						/>
+						<Line
+							type="monotone"
+							name="Biokaasu, alue 2"
+							dataKey="biogas.zone2"
+							stroke="yellow"
+						/>
+					</LineChart>
+				</ResponsiveContainer>
+			</div>
+			<div
+				style={{
+					display: 'flex',
+					alignItems: 'center',
+					justifyContent: 'center',
+					marginTop: 30,
+				}}
+			>
+				<select value={viewMode} onChange={changeViewMode}>
+					<option value="0">Viimeiset {daysToShow} päivää</option>
+					<option value="1">Kuukausien keskihinta</option>
+				</select>
+			</div>
+		</div>
+	);
+};
 
 export default App;
---
+++ src/utils/dataParser.js
@@ -0,0 +1,56 @@
+import { groupBy, uniqBy, slice } from 'lodash';
+import moment from 'moment';
+
+export const getPriceForDays = (data, daysToShow) => {
+	let items = data.map((item) => {
+		const date = moment(item.date).format('DD.MM.YYYY');
+		return { ...item, date };
+	});
+
+	// reverse the items
+	items = items.reverse();
+
+	// pick one per date
+	items = uniqBy(items, 'date');
+
+	// slice array by amount of days to show
+	const startIndex =
+		items.length - daysToShow >= 0 ? items.length - daysToShow : 0;
+	items = slice(items, startIndex, items.length);
+
+	return items;
+};
+
+export const getPricesPerMonth = (data) => {
+	let items = data.map((item) => {
+		const date = moment(item.date).format('MMMM YYYY');
+		return { ...item, date };
+	});
+
+	items = groupBy(items, 'date');
+	const months = Object.keys(items);
+	const monthData = months.map((month) => {
+		const res = {
+			date: month,
+			naturalgas: { zone1: 0.0, zone2: 0.0 },
+			biogas: { zone1: 0.0, zone2: 0.0 },
+		};
+		const noOfRecordsPerMonth = items[month].length;
+		items[month].forEach((item) => {
+			res.naturalgas.zone1 += item.naturalgas.zone1;
+			res.naturalgas.zone2 += item.naturalgas.zone2;
+			res.biogas.zone1 += item.biogas.zone1;
+			res.biogas.zone2 += item.biogas.zone2;
+		});
+		res.naturalgas.zone1 = (res.naturalgas.zone1 / noOfRecordsPerMonth).toFixed(
+			2
+		);
+		res.naturalgas.zone2 = (res.naturalgas.zone2 / noOfRecordsPerMonth).toFixed(
+			2
+		);
+		res.biogas.zone1 = (res.biogas.zone1 / noOfRecordsPerMonth).toFixed(2);
+		res.biogas.zone2 = (res.biogas.zone2 / noOfRecordsPerMonth).toFixed(2);
+		return res;
+	});
+	return monthData;
+};
